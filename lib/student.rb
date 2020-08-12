675a9a5e2a9e099b2ff819a2af8a085bc32d5c89
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-12 16:32:23 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+
+  def initialize
+    @knowledge = []  #initializes with an empty knowledge array
+  end
+
+  def learn(string)
+    @knowledge << string #  takes in an argument of a string of knowledge and adds it to the student's knowledge
+  end
+
+  def knowledge
+    @knowledge #returns that student's knowledge array
+  end
 
 end
