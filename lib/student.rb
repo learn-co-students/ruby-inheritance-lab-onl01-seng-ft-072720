e9962abece1f28d49b2dd713ba974534a98c8c34
Aaron Brady <aleebrady@gmail.com>
2020-08-13 20:06:18 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User # inherits from User class
+  
+  attr_accessor :knowledge # initializes with knowledge / setter/getter
+  
+  def initialize
+    @knowledge = [] # initializes with instance variable set to empty array
+  end
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,5 @@
+class User 
+  
+  attr_accessor :first_name, :last_name # creates a setter/getter method
+
+end
