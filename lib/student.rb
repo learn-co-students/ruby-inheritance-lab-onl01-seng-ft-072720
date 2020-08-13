c6d743691a1b90c73bd3adb00b8f5722b0f71640
Aaron Brady <aleebrady@gmail.com>
2020-08-13 20:10:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,8 @@ class Student < User # inherits from User class
     @knowledge = [] # initializes with instance variable set to empty array
   end
   
-  def learn(knowledge)
-    @knowledge << knowledge
+  def learn(knowledge) # takes in argument of knowledge string
+    @knowledge << knowledge # adds string to instance variable knowledge array
   end
   
 
